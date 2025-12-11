b51b98d62e0309a725d469f072923668138b2a0a
pabloinformatorio <pabloinformatorio@gmail.com>
2025-12-11 08:47:57 -0300

    Add URL patterns for LoginView, RegisterView, and LogoutView

--- blog/apps/user/urls.py
+++ blog/apps/user/urls.py
@@ -5,4 +5,7 @@ app_name = "user"
 
 urlpatterns = [
     path('users/profile', views.UserProfileView.as_view(), name="user-profile"),
+    path('auth/login', views.LoginView.as_view(), name="auth-login"),
+    path('auth/register', views.RegisterView.as_view(), name="auth-register"),
+    path('auth/logout', views.LogoutView.as_view(), name="auth-logout"),
 ]
